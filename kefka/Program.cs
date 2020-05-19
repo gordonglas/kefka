a941b11eb1c1cf593b948c0dcf7611e6552a4cf0
Gordon Glas <gglas@spigot.com>
2020-05-19 17:37:42 -0500

    cmdline

--- kefka/Program.cs
+++ kefka/Program.cs
@@ -6,7 +6,7 @@ namespace kefka
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // kefka --eol=lf path/to/file.txt -o output/path/
         }
     }
 }
---
+++ kefka/Source/CmdLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kefka.Source
+{
+    public class CmdLine
+    {
+        public CmdLine(string[] args)
+        {
+            
+        }
+    }
+}
